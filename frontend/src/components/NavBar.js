a6ee721c4ab14c697889624c78d9659da5c2f749
Avery King <avery.n.king@wmich.edu>
2021-12-13 21:32:52 -0500

    Navbar dropdown menu added

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -11,13 +11,26 @@ import {
     LogoutOutlined,
     MailOutlined,
     Notifications,
-    NotificationsOutlined,
+    NotificationsOutlined, PersonOutlined,
     SettingsOutlined
 } from "@mui/icons-material";
 import {IconButton} from "@mui/material";
 import App from "../App";
 import Snackbar from "./Snackbar";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
 const NavBar = ({loggedIn = false}) => {
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+
     const openLogin = () => {
         ReactDOM.render(<LoginModal display="1" />, document.getElementById("modal"))
     }
@@ -61,8 +74,22 @@ const NavBar = ({loggedIn = false}) => {
         )
     } else {
         return (
-
+<div>
+            <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'basic-button',
+                }}
+            >
+                <MenuItem onClick={handleClose}><PersonOutlined /> &nbsp;Profile</MenuItem>
+                <MenuItem onClick={handleClose}><SettingsOutlined /> &nbsp;Settings</MenuItem>
+                <MenuItem onClick={handleClose}><LogoutOutlined /> &nbsp;Logout</MenuItem>
+            </Menu>
             <nav className="navbar navbar-light navbar-expand-lg navigation-clean-button">
+
                 <div className="container"><a className="navbar-brand" href="#">Bloggit</a>
                     <button data-bs-toggle="collapse" className="navbar-toggler" data-bs-target="#navcol-1"><span
                         className="visually-hidden">Toggle navigation</span><span className="navbar-toggler-icon"></span>
@@ -77,14 +104,14 @@ const NavBar = ({loggedIn = false}) => {
                         <span className="navbar-text actions">
                             <IconButton className="navIcon" href="#"><Badge color="primary" badgeContent={2}><MailOutlined /></Badge></IconButton>
                             <IconButton className="navIcon" href="#"><Badge color="primary" badgeContent={4}><NotificationsOutlined /></Badge></IconButton>
-                            <IconButton className="navIcon" href="#"><ArrowDropDownCircleOutlined /></IconButton>
-                            <IconButton onClick={logout} className="navIcon" href="#"><LogoutOutlined /></IconButton>
+                            <IconButton onClick={handleClick} className="navIcon" href="#"><ArrowDropDownCircleOutlined /></IconButton>
+                            <IconButton onClick={logout} className="navIcon" href="#"></IconButton>
 
                         </span>
                     </div>
                 </div>
             </nav>
-
+</div>
         )
     }
 
