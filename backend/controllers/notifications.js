74629f67c53246e0c15aabf74e034ad1b90ece98
Avery King <avery.n.king@wmich.edu>
2021-12-15 06:45:46 -0500

    Return user notifcation count (/api/notifications/count/:user)

--- backend/controllers/notifications.js
+++ backend/controllers/notifications.js
@@ -1,21 +1,28 @@
 const notificationsRouter = require('express').Router()
 const Notification = require('../models/notification')
+const User = require('../models/users')
+notificationsRouter.get('/count/:user', async (request, response) => {
 
-notificationsRouter.get('/', (request, response) => {
-    response.send('notifications routed!')
+    await User.findById(request.params.user)
+        .then(user => response.json(user.notifications.length))
+        .catch(err => response.json({error:'invalid user'}))
+
+
+
 })
 
 notificationsRouter.post('/', async (request, response) => {
-
-
+//TODO: Prevent double requests from being sent, implement auth
+        console.log('in controller')
     const notification = new Notification({
         notificationData: {type:request.body.notificationType},
         userFrom: request.body.userFrom,
         userTo: request.body.userTo
     })
 
-    await notification.save().then(user => {
-        response.send(user)
+    await notification.save().then(notif => {
+        console.log(notif)
+        response.json(notif)
     }).catch(err => {
         console.log(err)
     })
