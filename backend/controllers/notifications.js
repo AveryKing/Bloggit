e4eb6e2862a38b06c03727b6637ba51a109a483c
Avery King <avery.n.king@wmich.edu>
2021-12-16 05:52:52 -0500

    notificationsService update - accept request fixed

--- backend/controllers/notifications.js
+++ backend/controllers/notifications.js
@@ -1,100 +1,88 @@
-const notificationsRouter = require('express').Router()
-const Notification = require('../models/notification')
-const User = require('../models/users')
+const notificationsRouter = require("express").Router();
+const Notification = require("../models/notification");
+const User = require("../models/users");
 
+notificationsRouter.get("/:user", async (request, response) => {
+    const arr = [];
+    await Notification.find({ userTo: request.params.user }).then(
+        (notification) => {
+            arr.push(notification);
+        }
+    );
 
-notificationsRouter.get('/:user', async (request, response) => {
-    const arr = []
-    await Notification.find({'userTo':request.params.user})
-        .then(notification => {
-            arr.push(notification)
-    })
+    response.json(arr);
+});
 
-    response.json(arr)
-})
-
-notificationsRouter.get('/count/:user', async (request, response) => {
-
+notificationsRouter.get("/count/:user", async (request, response) => {
     await User.findById(request.params.user)
-        .then(user => response.json({count:user.notifications.length}))
-        .catch(err => response.json({error:'invalid user'}))
+        .then((user) => response.json({ count: user.notifications.length }))
+        .catch((err) => response.json({ error: "invalid user" }));
+});
 
-
-
-})
-
-notificationsRouter.post('/accept', async (request, response) => {
-
-    let userFrom
-    let userTo
+notificationsRouter.post("/accept", async (request, response) => {
+    let userFrom;
+    let userTo;
     await Notification.findByIdAndRemove(request.body.id)
-        .then(async notif => {
-            userFrom = notif.userFrom
-            userTo = notif.userTo
-            await User.findById(userFrom).then(async user => {
-                await user.outgoingFriendRequests.pull(userTo)
-                     user.friends.push(userTo)
-                    user.save().then(async func => {
-                        await User.findById(userTo).then(async user => {
-                            await user.notifications.pull(request.body.id)
-                            user.friends.push(userFrom)
-                            user.save()
-                            response.json({'userAdded':userFrom})
-                        })
-                    })
-
-            }).catch(err => {
-                response.json({'error':'there was an error'})
-            })
-
+        .then(async (notif) => {
+            userFrom = notif.userFrom;
+            userTo = notif.userTo;
+            await User.findById(userFrom)
+                .then(async (user) => {
+                    await user.outgoingFriendRequests.pull(userTo);
+                    user.friends.push(userTo);
+                    user.save().then(async (func) => {
+                        await User.findById(userTo).then(async (user) => {
+                            await user.notifications.pull(request.body.id);
+                            user.friends.push(userFrom);
+                            user.save();
+                            response.json({ userAdded: userFrom });
+                        });
+                    });
+                })
+                .catch((err) => {
+                    response.json({ error: "there was an error" });
+                });
         })
-        .catch(err => {
-        response.json({'error':'there was an error'})
-    })
+        .catch((err) => {
+            response.json({ error: "there was an error" });
+        });
+});
 
+notificationsRouter.post("/", async (request, response) => {
+    //TODO: Prevent double requests from being sent, implement auth
 
-
-
-
-})
-
-notificationsRouter.post('/', async (request, response) => {
-//TODO: Prevent double requests from being sent, implement auth
-
-    let notificationMessage
-    await User.findById(request.body.userFrom).then(user => {
-            notificationMessage = `${user.username} sent you a friend request.`
-    })
-
+    let notificationMessage;
+    await User.findById(request.body.userFrom).then((user) => {
+        notificationMessage = `${user.username} sent you a friend request.`;
+    });
 
     const notification = new Notification({
         notificationData: {
-                            type:request.body.notificationType,
-                            message:notificationMessage
-                            },
+            type: request.body.notificationType,
+            message: notificationMessage,
+        },
         userFrom: request.body.userFrom,
-        userTo: request.body.userTo
-    })
+        userTo: request.body.userTo,
+    });
 
-    if(request.body.notificationType === 'friendRequest') {
-        const user = await User.findById(request.body.userFrom)
-        user.outgoingFriendRequests.push(request.body.userTo)
-        await user.save()
+    if (request.body.notificationType === "friendRequest") {
+        const user = await User.findById(request.body.userFrom);
+        user.outgoingFriendRequests.push(request.body.userTo);
+        await user.save();
     }
 
-    const user = await User.findById(request.body.userTo)
+    const user = await User.findById(request.body.userTo);
 
-    await notification.save().then(notif => {
+    await notification
+        .save()
+        .then((notif) => {
+            user.notifications = user.notifications.concat(notif.id);
+            user.save();
 
-       user.notifications = user.notifications.concat(notif.id)
-        user.save()
-
-        response.json(notif)
-    }).catch(err => {
-        console.log(err)
-    })
-
-
-
-})
-module.exports = notificationsRouter
+            response.json(notif);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+module.exports = notificationsRouter;
